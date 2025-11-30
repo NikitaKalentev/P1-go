acfa7028e5ad3717bf751b909ddcb9516604dae3
Nikita Kalentev <nicknamewest@yandex.ru>
2025-11-30 11:37:55 +0300

    5I

--- main.go
+++ main.go
@@ -1,128 +1,103 @@
-
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strconv"
+    "strings"
+    "time"
+)
+
+const (
+    serverURL = "http://srv.msk01.gigacorp.local/_stats"
+    loadAvgThreshold = 30.0
+    memoryUsageThreshold = 0.8
+    diskUsageThreshold = 0.9
+    networkUsageThreshold = 0.9
+    retryLimit = 3
+    pollInterval = 60 * time.Second // опрос каждые 60 секунд
 )
 
 func main() {
-	url := "http://srv.msk01.gigacorp.local/_stats"
-	errorCount := 0
-	maxErrors := 3
-
-	ticker := time.NewTicker(5 * time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		// Выполняем HTTP GET запрос
-		resp, err := http.Get(url)
-		if err != nil {
-			errorCount++
-			if errorCount >= maxErrors {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			continue
-		}
-
-		// Проверяем статус ответа
-		if resp.StatusCode != http.StatusOK {
-			errorCount++
-			resp.Body.Close()
-			if errorCount >= maxErrors {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			continue
-		}
-
-		// Читаем тело ответа
-		scanner := bufio.NewScanner(resp.Body)
-		if scanner.Scan() {
-			data := scanner.Text()
-			processStats(data)
-			// Сбрасываем счетчик ошибок при успешном запросе
-			errorCount = 0
-		} else {
-			errorCount++
-		}
-
-		resp.Body.Close()
-
-		if errorCount >= maxErrors {
-			fmt.Println("Unable to fetch server statistic")
-			return
-		}
-	}
+    errorCount := 0
+    
+    for {
+        resp, err := http.Get(serverURL)
+        if err != nil {
+            errorCount++
+            handleError(&errorCount)
+            continue
+        }
+        
+        defer resp.Body.Close()
+        
+        if resp.StatusCode != http.StatusOK {
+            errorCount++
+            handleError(&errorCount)
+            continue
+        }
+        
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            errorCount++
+            handleError(&errorCount)
+            continue
+        }
+        
+        stats := strings.Split(string(body), ",")
+        if len(stats) != 6 {
+            errorCount++
+            handleError(&errorCount)
+            continue
+        }
+        
+        errorCount = 0
+        
+        // Парсинг значений
+        loadAvg, _ := strconv.ParseFloat(stats[0], 64)
+        totalMem, _ := strconv.ParseFloat(stats[1], 64)
+        usedMem, _ := strconv.ParseFloat(stats[2], 64)
+        totalDisk, _ := strconv.ParseFloat(stats[3], 64)
+        usedDisk, _ := strconv.ParseFloat(stats[4], 64)
+        totalNetwork, _ := strconv.ParseFloat(stats[5], 64)
+        usedNetwork, _ := strconv.ParseFloat(stats[5], 64) // исправлено: должно быть stats[5]
+        
+        // Проверка Load Average
+        if loadAvg > loadAvgThreshold {
+            fmt.Printf("Load Average is too high: %.2f\n", loadAvg)
+        }
+        
+        // Проверка использования памяти
+        memUsage := usedMem / totalMem
+        if memUsage > memoryUsageThreshold {
+            fmt.Printf("Memory usage too high: %.2f%%\n", memUsage * 100)
+        }
+        
+        // Проверка использования диска
+        diskUsage := usedDisk / totalDisk
+        if diskUsage > diskUsageThreshold {
+            freeDisk := (totalDisk - usedDisk) / (1024 * 1024)
+            fmt.Printf("Free disk space is too low: %.2f Mb left\n", freeDisk)
+        }
+        
+        // Проверка использования сети
+        networkUsage := usedNetwork / totalNetwork
+        if networkUsage > networkUsageThreshold {
+            freeNetwork := (totalNetwork - usedNetwork) * 8 / 1000000
+            fmt.Printf("Network bandwidth usage high: %.2f Mbit/s available\n", freeNetwork)
+        }
+        
+        time.Sleep(pollInterval)
+    }
 }
 
-func processStats(data string) {
-	// Разделяем данные по запятым
-	values := strings.Split(strings.TrimSpace(data), ",")
-	if len(values) != 7 {
-		return // Неверный формат данных
-	}
-
-	// Парсим значения с проверкой ошибок
-	var loadAvg float64
-	var totalMem, usedMem, totalDisk, usedDisk, totalNet, usedNet uint64
-	var parseErr error
-
-	if loadAvg, parseErr = strconv.ParseFloat(values[0], 64); parseErr != nil {
-		return
-	}
-	if totalMem, parseErr = strconv.ParseUint(values[1], 10, 64); parseErr != nil {
-		return
-	}
-	if usedMem, parseErr = strconv.ParseUint(values[2], 10, 64); parseErr != nil {
-		return
-	}
-	if totalDisk, parseErr = strconv.ParseUint(values[3], 10, 64); parseErr != nil {
-		return
-	}
-	if usedDisk, parseErr = strconv.ParseUint(values[4], 10, 64); parseErr != nil {
-		return
-	}
-	if totalNet, parseErr = strconv.ParseUint(values[5], 10, 64); parseErr != nil {
-		return
-	}
-	if usedNet, parseErr = strconv.ParseUint(values[6], 10, 64); parseErr != nil {
-		return
-	}
-
-	// Проверяем Load Average
-	if loadAvg > 30 {
-		fmt.Printf("Load Average is too high: %.2f\n", loadAvg)
-	}
-
-	// Проверяем использование памяти
-	if totalMem > 0 {
-		memUsagePercent := float64(usedMem) / float64(totalMem) * 100
-		if memUsagePercent > 80 {
-			fmt.Printf("Memory usage too high: %.2f%%\n", memUsagePercent)
-		}
-	}
-
-	// Проверяем свободное место на диске
-	if totalDisk > 0 {
-		diskUsagePercent := float64(usedDisk) / float64(totalDisk) * 100
-		if diskUsagePercent > 90 {
-			freeSpaceMB := float64(totalDisk-usedDisk) / (1024 * 1024)
-			fmt.Printf("Free disk space is too low: %.2f Mb left\n", freeSpaceMB)
-		}
-	}
-
-	// Проверяем использование сети
-	if totalNet > 0 {
-		netUsagePercent := float64(usedNet) / float64(totalNet) * 100
-		if netUsagePercent > 90 {
-			availableBandwidthMbit := float64(totalNet-usedNet) * 8 / (1000 * 1000)
-			fmt.Printf("Network bandwidth usage high: %.2f Mbit/s available\n", availableBandwidthMbit)
-		}
-	}
+// Функция обработки ошибок
+func handleError(errorCount *int) {
+    *errorCount++
+    if *errorCount >= retryLimit {
+        fmt.Println("Unable to fetch server statistic")
+        *errorCount = 0
+    }
+    time.Sleep(pollInterval)
 }
